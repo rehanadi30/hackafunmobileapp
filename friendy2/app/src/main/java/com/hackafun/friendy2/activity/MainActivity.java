faf1dbf21983d0087964ee3a0075bcbec6e3396f
rehanadi30 <47605698+rehanadi30@users.noreply.github.com>
2024-08-17 18:15:51 +1000

    fix switch case

--- friendy2/app/src/main/java/com/hackafun/friendy2/activity/MainActivity.java
+++ friendy2/app/src/main/java/com/hackafun/friendy2/activity/MainActivity.java
@@ -1,29 +1,61 @@
 package com.hackafun.friendy2.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hackafun.friendy2.R;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton ib;
+    private ImageButton ib;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize ImageButton and set its onClickListener
         ib = findViewById(R.id.imageButtonMap);
         ib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, MapActivity.class);
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // Initialize BottomNavigationView and set its onNavigationItemSelectedListener
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_chat:
+                        Intent chatIntent = new Intent(MainActivity.this, ChatActivity.class);
+                        startActivity(chatIntent);
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.nav_home:
+                        // Handle home action
+                        return true;
+
+                    case R.id.nav_friends:
+                        Intent friendsIntent = new Intent(MainActivity.this, FriendActivity.class);
+                        startActivity(friendsIntent);
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    default:
+                        return false;
+                }
             }
         });
     }
