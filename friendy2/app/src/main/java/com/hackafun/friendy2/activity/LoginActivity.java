9a40196a10a678de42cb2527262b8860675b7c4e
rehanadi30 <47605698+rehanadi30@users.noreply.github.com>
2024-08-18 13:52:46 +1000

    add register and query activity

--- friendy2/app/src/main/java/com/hackafun/friendy2/activity/LoginActivity.java
+++ friendy2/app/src/main/java/com/hackafun/friendy2/activity/LoginActivity.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.hackafun.friendy2.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -30,6 +34,27 @@ public class LoginActivity extends AppCompatActivity {
             // User is not logged in, continue with the login screen
             setContentView(R.layout.activity_login);
             // Additional login setup if needed
+
+            Button buttonLogin = findViewById(R.id.loginButton);
+            buttonLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent chatIntent = new Intent(LoginActivity.this, MainEmptyActivity.class);
+                    startActivity(chatIntent);
+                    overridePendingTransition(0, 0);
+                    finish();
+                }
+            });
+
+            TextView register = findViewById(R.id.createAccountTextView);
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent chatIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(chatIntent);
+                    overridePendingTransition(0, 0);
+                }
+            });
         }
     }
 }
